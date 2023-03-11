9c16db63a33518d9945df6228d10e4780a426688
saimrinaal <89908644+saimrinaal@users.noreply.github.com>
2023-03-11 15:53:26 +0530

    Update script.js

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -10,7 +10,6 @@ wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
 window.onload = ()=>{
-    // make sure all the boxes in the board are clickable
     for (let i = 0; i < allBox.length; i++) {
        allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
@@ -29,9 +28,7 @@ selectBtnO.onclick = ()=>{
 
 let playerXIcon = "fas fa-times", playerOIcon = "far fa-circle", playerSign = "X", runBot = true;
 
-// user interaction with the board
 function clickedBox(element){
-    // console.log("Clicked")
     if(players.classList.contains("player")){
         playerSign = "O";
         element.innerHTML = `<i class="${playerOIcon}"></i>`;
@@ -47,7 +44,6 @@ function clickedBox(element){
     element.style.pointerEvents = "none";
     playBoard.style.pointerEvents = "none";
 
-    // buffer time to pretend that the AI's thinking
     let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
     setTimeout(()=>{
         bot(runBot);
@@ -56,12 +52,10 @@ function clickedBox(element){
 
 }
   
-// computer interaction with the board
 function bot(){
     let array = [];
     if(runBot){
         playerSign = "O";
-        // find the remaining boxes that has not been marked
         for (let i = 0; i < allBox.length; i++) {
             if(allBox[i].childElementCount == 0){
                 array.push(i);
@@ -88,24 +82,20 @@ function bot(){
         playerSign = "X";        
     }
 }
-// get the sign of a certain box
 function getIdVal(classname){
     return document.querySelector(".box" + classname).id;
 }
-// check 3 tiles to see if they are the same
 function checkIdSign(val1, val2, val3, sign){ 
     if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
         return true;
     }
     return false;
 }
-// check winner
 function selectWinner(){
     if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
         runBot = false;
         bot(runBot);
 
-        // buffer time
         setTimeout(()=>{
             resultBox.classList.add("show");
             playBoard.classList.remove("show");
@@ -113,12 +103,10 @@ function selectWinner(){
         wonText.innerHTML = `Player ${playerSign}<br> won the game!`;
     }
     else{
-        // if the board is full
         if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
             runBot = false;
             bot(runBot);
             
-            // buffer time for showing the match has been drawn
             setTimeout(()=>{
                 resultBox.classList.add("show");
                 playBoard.classList.remove("show");
@@ -128,7 +116,6 @@ function selectWinner(){
     }
 }
 
-// reload page when replay button is clicked
 replayBtn.onclick = () => {
     window.location.reload();
 }
